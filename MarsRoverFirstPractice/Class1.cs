1d2707b069ab2e34a4a4de18b3174e325d8ff153
jonathan.allen <jonathan.allen@ENGR4HCP5S3>
2023-01-09 11:23:23 -0700

    Add project files.

---
+++ MarsRoverFirstPractice/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace MarsRoverFirstPractice
+{
+    public class Class1
+    {
+         static public (Point,String) simulateRover( String map, String directionCommands)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MarsRoverTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using MarsRoverFirstPractice;
+
+namespace MarsRoverTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var map = "\U0001f7e9\U0001f7e9🌳\U0001f7e9\U0001f7e9\r\n\U0001f7e9\U0001f7e9\U0001f7e9\U0001f7e9\U0001f7e9\r\n\U0001f7e9\U0001f7e9\U0001f7e9🌳\U0001f7e9\r\n\U0001f7e9🌳\U0001f7e9\U0001f7e9\U0001f7e9\r\n➡️\U0001f7e9\U0001f7e9\U0001f7e9\U0001f7e9"
+            Class1.simulateRover();
+            Assert.Pass();
+        }
+    }
+}
