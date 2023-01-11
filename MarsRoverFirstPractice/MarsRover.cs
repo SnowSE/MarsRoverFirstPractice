a88a554bf7402885ccfec2b8c140beddcb086f57
Jayse19 <Jayse19@hotmail.com>
2023-01-10 21:10:43 -0700

    Rover can move

---
+++ MarsRoverFirstPractice/MarsRover.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace MarsRoverFirstPractice
+{
+    public class MarsRover
+    {
+        public int x;
+        public int y;
+        public string direction;
+        public MarsRover(string location)
+        {
+            int.TryParse(location.Split(" ")[0], out x);
+            int.TryParse(location.Split(" ")[1], out y);
+            direction = location.Split(" ")[2];
+        }
+        public void RotateLeft()
+        {
+            switch(direction)
+            {
+                case "N":
+                    direction = "W";
+                    break;
+                case "E":
+                    direction = "N";
+                    break;
+                case "S":
+                    direction = "E";
+                    break;
+                case "W":
+                    direction = "S";
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+        }
+        public void RotateRight()
+        {
+            switch (direction)
+            {
+                case "N":
+                    direction = "E";
+                    break;
+                case "E":
+                    direction = "S";
+                    break;
+                case "S":
+                    direction = "W";
+                    break;
+                case "W":
+                    direction = "N";
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+        }
+        public void MoveForward()
+        {
+            switch (direction)
+            {
+                case "N":
+                    y += 1;
+                    break;
+                case "E":
+                    x += 1;
+                    break;
+                case "S":
+                    y -= 1;
+                    break;
+                case "W":
+                    x -= 1;
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+        }
+        public void Move(string moveRoverCommands)
+        {
+            char[] instructions = moveRoverCommands.ToCharArray();
+
+            for (int i = 0; i < instructions.Length; i++)
+            {
+                switch (instructions[i])
+                {
+                    case 'L':
+                        RotateLeft();
+                        break;
+                    case 'R':
+                        RotateRight();
+                        break;
+                    case 'F':
+                        MoveForward();
+                        break;
+                    default:
+                        throw new ArgumentException();
+                }
+            }
+        }
+    }
+}
--- MarsRoverTest/UnitTest1.cs
+++ MarsRoverTest/UnitTest1.cs
@@ -10,11 +10,32 @@ namespace MarsRoverTest
         }
 
         [Test]
-        public void Test1()
+        public void RotateLeft()
         {
-            var map = "\U0001f7e9\U0001f7e9🌳\U0001f7e9\U0001f7e9\r\n\U0001f7e9\U0001f7e9\U0001f7e9\U0001f7e9\U0001f7e9\r\n\U0001f7e9\U0001f7e9\U0001f7e9🌳\U0001f7e9\r\n\U0001f7e9🌳\U0001f7e9\U0001f7e9\U0001f7e9\r\n➡️\U0001f7e9\U0001f7e9\U0001f7e9\U0001f7e9"
-            Class1.simulateRover();
-            Assert.Pass();
+            MarsRover rover = new MarsRover("0 0 W");
+            rover.RotateLeft();
+            Assert.AreEqual("S", rover.direction);
+        }
+        [Test]
+        public void RotateRight()
+        {
+            MarsRover rover = new MarsRover("0 0 W");
+            rover.RotateRight();
+            Assert.AreEqual("N", rover.direction);
+        }
+        [Test]
+        public void MoveForward()
+        {
+            MarsRover rover = new MarsRover("1 2 N");
+            rover.MoveForward();
+            Assert.AreEqual(3, rover.y);
+        }
+        [Test]
+        public void RoverShouldMove()
+        {
+            MarsRover rover = new MarsRover("1 2 N");
+            rover.Move("LFLFLFLFF");
+            Assert.AreEqual("1 3 N", rover.x + " " + rover.y + " " + rover.direction);
         }
     }
 }
