41158f715eeb41f6f31508b81928a69069a5eb0d
jm <madsencad@gmail.com>
2023-01-12 11:52:44 -0700

    Finished, Note I always start at (0,0) and the (0,0) position on the map has the starting direction

--- MarsRoverFirstPractice/MarsRover.cs
+++ MarsRoverFirstPractice/MarsRover.cs
@@ -1,16 +1,16 @@
 ﻿using System.Drawing;
+using System.Runtime.Intrinsics.X86;
 
 namespace MarsRoverFirstPractice
 {
     public class MarsRover
     {
-         static public (Point,String) simulateRover(String map, String directionCommands)
+         static public (Point, string) simulateRover(string map, string directionCommands)
         {
             var north = "⬆️";
             var south = "⬇️";
             var east = "➡️";
             var west = "⬅️";
-
 
             //if (directionCommands == "")
             //    throw new Exception("Direction commands are empty");
@@ -18,26 +18,25 @@ namespace MarsRoverFirstPractice
             var mapArray = map.Split(" ");
             var direction = mapArray[0];
             var position = new Point(0, 0);
-            
+            List<Point> obstacles = new List<Point>();
+
             int gridSize = (int)Math.Sqrt(mapArray.Length);
 
             for (int i = 1; i < mapArray.Length; i++)
             {
                 if (mapArray[i] == "X")
                 {
-                    
-                    position.X = (i - 1) % gridSize;
-                    position.Y = (i - 1) / gridSize;
+                    obstacles.Add(new Point(i % gridSize, i / gridSize));
                 }
             }
 
             var directionCommandsArray = directionCommands.Split(" ");
             foreach (var directionCommand in directionCommandsArray)
             {
-                
+                var previousPostion = position;
                 if (directionCommand == "F")
                 {
-                    if (direction == north && mapArray[1] != "X")
+                    if (direction == north)
                         position.Y += 1;
                     else if (direction == south)
                         position.Y -= 1;
@@ -79,15 +78,20 @@ namespace MarsRoverFirstPractice
                     else if (direction == west)
                         direction = north;
                 }
-                
-                
 
-            }
+                //obstacle check
+                if (obstacles.Contains(position))
+                {
+                    position = previousPostion;
+                }
 
-            
+                //out of bounds check(remain in position if try leave map)
+                if (position.X < 0 || position.X > gridSize || position.Y < 0 || position.Y > gridSize)
+                {
+                    position = previousPostion;
+                }
+            }           
             return (position, direction);
-        }
-        
-        
+        }       
     }
 }
--- MarsRoverTest/UnitTest1.cs
+++ MarsRoverTest/UnitTest1.cs
@@ -10,22 +10,25 @@ namespace MarsRoverTest
         {
         }
 
-        //[Test]
-        //public void DirectionEmtpy()
-        //{
-        //    var map = "";
-        //    var directionCommands = "";
-        //    var exception = Assert.Throws<Exception>(() => MarsRover.simulateRover(map, directionCommands));
-        //    Assert.That("Direction commands are empty" == exception.Message);
-        //}
-
         [Test]
-        public void RoverMoveOneForward()
+        public void RoverMove()
         {
             var map = "➡️ 0 0 0";
             var directionCommands = "F";
             var result = MarsRover.simulateRover(map, directionCommands);
             Assert.That(result.Item1.X == 1);
+            Assert.That(result.Item1.Y == 0);
+            Assert.That(result.Item2 == "➡️");
+
+            directionCommands = "F F";
+            result = MarsRover.simulateRover(map, directionCommands);
+            Assert.That(result.Item1.X == 2);
+            Assert.That(result.Item1.Y == 0);
+            Assert.That(result.Item2 == "➡️");
+
+            directionCommands = "F B";
+            result = MarsRover.simulateRover(map, directionCommands);
+            Assert.That(result.Item1.X == 0);
             Assert.That(result.Item1.Y == 0);
             Assert.That(result.Item2 == "➡️");
         }
@@ -39,9 +42,115 @@ namespace MarsRoverTest
             Assert.That(result.Item1.X == 0);
             Assert.That(result.Item1.Y == 0);
             Assert.That(result.Item2 == "➡️");
-
         }
 
+        [Test]
+        public void RoverMoveOneOutOfBounds()
+        {
+            var map = "⬇️ X 0 0";
+            var directionCommands = "F";
+            var result = MarsRover.simulateRover(map, directionCommands);
+            Assert.That(result.Item1.X == 0);
+            Assert.That(result.Item1.Y == 0);
+            Assert.That(result.Item2 == "⬇️");
+
+            map = "⬅️ X 0 0";
+            result = MarsRover.simulateRover(map, directionCommands);
+            Assert.That(result.Item1.X == 0);
+            Assert.That(result.Item1.Y == 0);
+            Assert.That(result.Item2 == "⬅️");
+        }
+
+        [Test]
+        public void RoverRotateRight()
+        {
+            var map = "⬆️ 0 0 0";
+            var directionCommands = "R";
+            var result = MarsRover.simulateRover(map, directionCommands);
+            Assert.That(result.Item1.X == 0);
+            Assert.That(result.Item1.Y == 0);
+            Assert.That(result.Item2 == "➡️");
+
+            map = "➡️ 0 0 0";
+            result = MarsRover.simulateRover(map, directionCommands);
+            Assert.That(result.Item1.X == 0);
+            Assert.That(result.Item1.Y == 0);
+            Assert.That(result.Item2 == "⬇️");
+
+            map = "⬇️ 0 0 0";
+            result = MarsRover.simulateRover(map, directionCommands);
+            Assert.That(result.Item1.X == 0);
+            Assert.That(result.Item1.Y == 0);
+            Assert.That(result.Item2 == "⬅️");
+
+            map = "⬅️ 0 0 0";
+            result = MarsRover.simulateRover(map, directionCommands);
+            Assert.That(result.Item1.X == 0);
+            Assert.That(result.Item1.Y == 0);
+            Assert.That(result.Item2 == "⬆️");
+        }
+
+        [Test]
+        public void RoverRotateLeft()
+        {
+            var map = "⬆️ 0 0 0";
+            var directionCommands = "L";
+            var result = MarsRover.simulateRover(map, directionCommands);
+            Assert.That(result.Item1.X == 0);
+            Assert.That(result.Item1.Y == 0);
+            Assert.That(result.Item2 == "⬅️");
+
+            map = "➡️ 0 0 0";
+            result = MarsRover.simulateRover(map, directionCommands);
+            Assert.That(result.Item1.X == 0);
+            Assert.That(result.Item1.Y == 0);
+            Assert.That(result.Item2 == "⬆️");
+
+            map = "⬇️ 0 0 0";
+            result = MarsRover.simulateRover(map, directionCommands);
+            Assert.That(result.Item1.X == 0);
+            Assert.That(result.Item1.Y == 0);
+            Assert.That(result.Item2 == "➡️");
+
+            map = "⬅️ 0 0 0";
+            result = MarsRover.simulateRover(map, directionCommands);
+            Assert.That(result.Item1.X == 0);
+            Assert.That(result.Item1.Y == 0);
+            Assert.That(result.Item2 == "⬇️");
+        }
+
+        [Test]
+        public void RoverMoveAndRoate()
+        {
+            var map = "⬆️ 0 0 0";
+            var directionCommands = "R F";
+            var result = MarsRover.simulateRover(map, directionCommands);
+            Assert.That(result.Item1.X == 1);
+            Assert.That(result.Item1.Y == 0);
+            Assert.That(result.Item2 == "➡️");
+
+            map = "➡️ 0 0 0";
+            directionCommands = "L F";
+            result = MarsRover.simulateRover(map, directionCommands);
+            Assert.That(result.Item1.X == 0);
+            Assert.That(result.Item1.Y == 1);
+            Assert.That(result.Item2 == "⬆️");            
+        }
+
+        [Test]
+        public void RoverBigMapMoveAndRotate()
+        {
+            // 0 X 0 0
+            // 0 0 0 0
+            // X 0 X 0
+            // ⬆️ 0 0 0            
+            var map = "⬆️ 0 0 0 X 0 X 0 0 0 0 0 0 X 0 0";
+            var directionCommands = "R F F F L F F F L F";
+            var result = MarsRover.simulateRover(map, directionCommands);
+            Assert.That(result.Item1.X == 2);
+            Assert.That(result.Item1.Y == 3);
+            Assert.That(result.Item2 == "⬅️");
+        }
     }
 
 }
